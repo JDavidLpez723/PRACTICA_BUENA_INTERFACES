88182eb64b39c555b80eb75ec48a43ccd46c9254
JDavidLpez723 <75070648+JDavidLpez723@users.noreply.github.com>
2020-12-15 16:10:18 +0100

    Formulario registro y guardar cookie
    Por favor no liarla

--- script/inicio.s
+++ script/inicio.s
@@ -5,6 +5,9 @@ $(document).ready(function start2(){
     $("#inicio").hide();
     $("#formulario").show();$("#formulario").css("display", "grid");
   });
+  $("#save").click(function(){
+    checkEmail();
+  });
   
   $("#alumno").click(function(){
     $(".grado").show();
@@ -41,44 +44,29 @@ function getCookie(name) { //para obtener los distintos Strings de la cookie y e
 }
 
 function checkCookie(a) { //validamos formulario. Parametro: valor de contador. Si esta todo correcto, creamos cookie
-
 	var delimiter = "---";
 
-    username = document.getElementById('username').value;
+    username = document.getElementById('usuario').value;
     if (username == "") {
         alert("Debe rellenar el usuario");
         return "";
     }
-	
 	nia = document.getElementById('nia').value;
 	if (nia == "") {
         alert("Debe rellenar con su NIA");
         return "";
     }
-	
-	
-    password = document.getElementById('password').value;
+    password = document.getElementById('contraseña').value;
     if (password == "") {
         alert("Debe rellenar la contraseña");
         return "";
     }
-	
-    name = document.getElementById('name').value;
-    if (name == "") {
+    namee = document.getElementById('nomAp').value;
+    if (namee == "") {
         alert("Debe rellenar el nombre");
         return "";
     }
-    surname_1 = document.getElementById('surname_1').value;
-    if (surname_1 == "") {
-        alert("Debe rellenar el primer apellido");
-        return "";
-    }
-    surname_2 = document.getElementById('surname_2').value;
-    if (surname_2 == "") {
-        alert("Debe rellenar el segundo apellido");
-        return "";
-    }
-    email = document.getElementById('email').value;
+    email = document.getElementById('correo').value;
     if (email == "") {
         alert("Debe rellenar un email correcto");
         return "";
@@ -88,8 +76,7 @@ function checkCookie(a) { //validamos formulario. Parametro: valor de contador. 
         alert("Debe rellenar un email correcto, formato nombre@dominio.extensión");
         return "";
     }
-
-    bday = document.getElementById('bday').value;
+    bday = document.getElementById('birth').value;
     if (bday == "") {
         alert("Debe rellenar fecha de nacimiento");
         return "";
@@ -99,7 +86,7 @@ function checkCookie(a) { //validamos formulario. Parametro: valor de contador. 
         alert("Debe rellenar su DNI");
         return "";
     }
-	
+	/*
 	rol = document.getElementById('rol').value;
     if (rol == "") {
         alert("Debe indicar un rol");
@@ -118,15 +105,15 @@ function checkCookie(a) { //validamos formulario. Parametro: valor de contador. 
     if (document.getElementById('aleman').checked) idioma_1 = document.getElementById('aleman').value;
     idioma_6 = "";
     if (document.getElementById('portugues').checked) idioma_1 = document.getElementById('portugues').value;
-    conditions = document.getElementById('conditions').value;
+    */
+    conditions = document.getElementById('terms').value;
+    
     if (conditions == "") {
         alert("No has aceptado los términos");
         return "";
     }
-
     //almacenamos en un String los valores de los campos separados por delimitador
-    var userdata = username + delimiter + nia + delimiter + password + delimiter + name + delimiter + surname_1 + delimiter + surname_2 + delimiter + email + delimiter + bday + delimiter + dni + delimiter + rol + delimiter 
-         + idioma_1 + delimiter + idioma_2 + delimiter + idioma_3 + delimiter + idioma_4 + delimiter + idioma_5 + delimiter + idioma_6 + delimiter + conditions;
+    var userdata = username + delimiter + nia + delimiter + password + delimiter + namee + delimiter + email + delimiter + bday + delimiter + dni + delimiter + conditions;
 
     counter = parseInt(a, 10); //pasamos a entero el contador
     counter += 1; //lo incrementamos para llamar a la proxima cookie
@@ -136,11 +123,29 @@ function checkCookie(a) { //validamos formulario. Parametro: valor de contador. 
     sessionStorage.setItem(name_cookie, name_cookie); //para tener sólo id de cookie guardada y buscar luego en iniciar sesion
 
     //si todo esta correcto
-	
-	if (confirm("Registro realizado con éxito")){
-		//Que aparezca iniciar session
-		document.getElementById('form_inicio').style.display='block'
-		//Que se quite lo de registrarse
-		document.getElementById('form_user').style.display='none'
-	} 
+
+	alert("Registro realizado con éxito");
+    
 }
+
+function checkEmail() { //comprobamos primero si email esta correcto
+    var delimiter = "---";
+    if (sessionStorage.getItem("contador") == null) //sólo en la primera ejecucion establecemos contador, para posteriormente nombrar la cookie y buscar por ella
+        sessionStorage.setItem("contador", "0");
+    
+    email = document.getElementById('correo').value;
+    if (email == "") return false; //si email no tiene valor, no tiene sentido que busque si existe
+    for (data_toSearch = document.cookie.split("delimiter"), i = data_toSearch.length; i--;) //si existe, no se hace nada
+        if (data_toSearch[i] == email) {
+            alert("Ya existe cuenta asociada a este correo");
+            return false;
+        }
+
+
+    checkCookie(sessionStorage.getItem('contador')); //llamamos a checkCookie para rellenar la cookie con los valores de los campos del form y despues crearla
+
+
+
+}
+
+
